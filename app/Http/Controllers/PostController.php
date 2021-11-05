c295a6388f1ab3e4f5b8b3f24780d4c09c4735a4
radytrainer <rady.y@passerellesnumeriques.org>
2021-11-05 21:50:44 +0700

    Route Part 2

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+    
+        return response()->json(['message' => "Post Created"], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Post::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+    $post->title = $request->title;
+    $post->description = $request->description;
+    $post->save();
+
+    return response()->json(['message' => "Post Updated"], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Post::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,51 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
+use App\Http\Controllers\PostController;
 
-Route::get('text', function() {
-    return "Hello PNC Laravel 8";
-});
-
-Route::get('person/{name}', function($name) {
-    return "Good morning ". $name;
-});
-
-Route::get('person/{name}/{age}', function($name, $age) {
-    return "Good morning ". $name . " & my age : ". $age;
-});
-
-Route::get('posts', function() {
-    return Post::all();
-});
-
-Route::get('posts/{id}', function($id) {
-    return Post::findOrFail($id);
-});
-
-
-Route::post('posts', function(Request $request) {
-    $post = new Post();
-    $post->title = $request->title;
-    $post->description = $request->description;
-    $post->save();
-
-    return response()->json(['message' => "Post Created"], 201);
-});
-
-Route::put('posts/{id}', function(Request $request, $id) {
-    $post = Post::findOrFail($id);
-    $post->title = $request->title;
-    $post->description = $request->description;
-    $post->save();
-
-    return response()->json(['message' => "Post Updated"], 200);
-});
-
-Route::delete('posts/{id}', function($id) {
-    return Post::destroy($id);
-});
-
+Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/{id}', [PostController::class, 'show']);
+Route::post('posts', [PostController::class, 'store']);
+Route::put('posts/{id}', [PostController::class, 'update']);
+Route::delete('posts/{id}', [PostController::class, 'destroy']);
 
 Route::fallback(function() {
     return "Your Route Laravel Not Found";
